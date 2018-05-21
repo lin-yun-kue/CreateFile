5b63368a92fc5ae62cffd6a87d3eac326afe32a4
GIT4U\Kyle.Lin <kyle.lin@git4u.com>
2018-05-21 13:06:03 +0800

    Create File & Get TempPath

---
+++ LogFile/LogFile/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var tempPath = Path.GetTempPath();
+
+            var year = DateTime.Now.Year;
+            var month = DateTime.Now.Month;
+            var date = DateTime.Now.Day;
+
+            tempPath += $"{year}{month}{date}_KyleLin.txt";
+
+            using (var fileStream = File.Create(tempPath))
+            {
+                var info = new UTF8Encoding(true).GetBytes("This is some text in the file.");
+                fileStream.Write(info, 0, info.Length);
+            }
+
+                Console.ReadLine();
+        }
+    }
+}
